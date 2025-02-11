db9b9d6da9a6644b82fc5fdf69dcd1af3a26b7d4
Sten-Armin Mahlapuu <50709197064@tptlive.ee>
2025-02-11 10:31:17 +0200

    Simple switch

--- kasparitund/src/App.vue
+++ kasparitund/src/App.vue
@@ -1,15 +1,22 @@
 <script setup>
+import { ref } from 'vue';
 import tabs from './components/tabs.vue';
 let titles = ['Item1', 'Item2', 'Item3', 'Item4'];
- 
+let contents = ['content 1', 'content 2', 'content 3', 'content 4'];
+let activeTab = ref(0);
+function change(key){
+    console.log(key);
+    activeTab.value = key;
+} 
+
 </script>
  
 <template>
-    <Tabs :item="titles"></Tabs>
-
+    <Tabs :item="titles" @change="change"></Tabs>
+    <div class="container">
+        <h1>{{ contents[activeTab] }}</h1>
+    </div>
 </template>
  
 <style>
-
-
 </style>
