02bf2d39f6c6a8517477f2b0f8796a95d6113dd0
S73n <50709197064@tptlive.ee>
2025-02-18 11:28:29 +0200

    Add stuff

--- kasparitund/src/pages/Chuck.vue
+++ kasparitund/src/pages/Chuck.vue
@@ -1,13 +1,12 @@
 <script setup>
 import { ref } from 'vue';
-
+import axios from 'axios';
 
 let joke = ref('');
 
-let response = await fetch('https://api.chucknorris.io/jokes/random');
-let data = await response.json();
-console.log(data);
-joke.value = data.value;
+let response = await axios.get('https://api.chucknorris.io/jokes/random');
+joke.value = response.data.value;
+
 </script>
 
 <template>
