b942f78ff7ce583021289009138b1933d98141b9
S73n <50709197064@tptlive.ee>
2025-05-13 10:15:00 +0300

    Add fixes

--- kasparitund/src/pages/Chat.vue
+++ kasparitund/src/pages/Chat.vue
@@ -3,30 +3,30 @@ import axios from 'axios';
 import { ref } from 'vue';
 
 let messages = ref([]);
-let input = ref('');
+let input = ref(''); 
 
 // Create WebSocket connection.
 const socket = new WebSocket("ws://localhost:8080");
 
 // Connection opened
 socket.addEventListener("open", (event) => {
-    // socket.send("Hello Server!");
+  //socket.send("Hello Server!");
 });
 
 // Listen for messages
 socket.addEventListener("message", (event) => {
-    console.log("Message from server", event.data);
-    let data = JSON.parse(event.data);
-    if (data.type === 'messages') {
-        messages.value = data.messages;
-    } else if (data.type === 'message') {
-        messages.value.push(data);
-    }
+  console.log("Message from server ", event.data);
+  let data = JSON.parse(event.data);
+  if(data.type === 'messages'){
+    messages.value = data.messages;
+  } else if (data.type === 'message') {
+    messages.value.push(data);
+  }
 });
 
-async function send() {
-    socket.send(JSON.stringify({ type: 'message', message: input.value }));
-    input.value = '';
+async function send(){
+  socket.send(JSON.stringify({type:'message', message: input.value}));
+  input.value = '';
 }
 </script>
 
