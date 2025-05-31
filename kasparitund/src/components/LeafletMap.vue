041557c83026518709b4c2ebad403a6a03265924
S73n <sten.armin@gmail.com>
2025-05-31 11:02:17 +0300

    Add go to home button 'top right' on Leaflet

--- kasparitund/src/components/LeafletMap.vue
+++ kasparitund/src/components/LeafletMap.vue
@@ -9,17 +9,42 @@ import { onMounted, useId, watch } from 'vue';
 
 const {center, zoom} = defineProps(['center', 'zoom']);
 const id = 'map-' + useId();
+const homeCoords = [59.30737833793286, 24.666343408873033];
 let map;
+
 onMounted(() => {
     map = L.map(id).setView(center, zoom);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
-    var marker = L.marker([59.4269, 24.7434]).addTo(map);
+    
+    // Marker
+    L.marker(homeCoords).addTo(map).bindPopup("Kodu");
+    
+    // Polygon
+    const homePolygon = L.circle(homeCoords, {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.3,
+        radius: 200
+    }).addTo(map);
+    
+    const goHomeControl = L.Control.extend({
+        onAdd: function(map) {
+            const btn = L.DomUtil.create('button', 'leaflet-bar leaflet-control');
+            btn.innerHTML = '🏠 Go to Home';
+            btn.style.backgroundColor = 'white';
+            btn.style.padding = '5px 10px';
+            btn.style.cursor = 'pointer';
+            btn.onclick = () => map.setView(homeCoords, 16);
+            return btn;
+        }
+    });
+    new goHomeControl({ position: 'topright' }).addTo(map);
 });
-watch(() => center, (center, oldCenter) => {
-    console.log(center, oldCenter);
+
+watch(() => center, (center) => {
     map.panTo(center);
 });
 
@@ -27,9 +52,11 @@ watch(() => zoom, (zoom) => {
     map.setZoom(zoom);
 });
 </script>
+
 <template>
-     <div :id="id"></div>
+    <div :id="id"></div>
 </template>
+
 <style scoped>
 div { 
     height: 90vh;
